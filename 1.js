01af7bb57535a4aec58e0dc015f4c7a105e3d2af
badat <you@example.com>
2020-02-20 08:35:39 +0700

    baitap NamNhuan

---
+++ 1.js
@@ -0,0 +1,15 @@
+let year =  parseInt(prompt("nhap year:"));
+if (year % 4 == 0) {
+    if (year % 100 == 0) {
+        if (year % 400 == 0) {
+            alert(year + " is a leap year");
+        } else {
+            alert(year + " is NOT a leap year");
+        }
+    } else {
+        alert(year + " is a leap year");
+    }
+} else {
+    alert(year + " is NOT a leap year");
+}
+
